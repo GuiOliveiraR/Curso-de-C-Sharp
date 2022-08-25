2428ff1617ea94f1fa15d2310ed3b059906307c0
guilherme.orodrigue3 <guilherme.orodrigue3@L130570788W10-1>
2022-08-25 15:03:46 -0300

    Adicionar arquivos de projeto.

---
+++ Ex.04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fazer um programa para calcular o aumento de salário
+            de um funcionario x exibindo
+
+            - % de aumneto do salário
+            - Aumento em reais
+            - Salário Final
+            */
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("=== Programa de aumento de salário ===");
+            Console.WriteLine("======================================");
+
+            Console.WriteLine("Insira o nome do funcionário: ");
+            string Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salário do funcionário");
+            double SalarioInicial = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual % de aumento deseja para o funcionário " + Nome + "?");
+        }
+    }
+}
---
+++ ex.01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Agora estou Programando");
+            Console.WriteLine("Guilherme de Oliveira Rodrigues");
+            Console.WriteLine("Vai Corinthians PORRA");
+        }
+    }
+}
